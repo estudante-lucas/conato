be55be2f4e73662609553186ee69772e67b4e19b
Lucas Gustavo Hillesheim <lucas_hillesheim@estudante.sc.senai.br>
2023-04-08 14:42:15 -0300

    first commit

---
+++ src/test/java/com/senai/contato/ContatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.contato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
